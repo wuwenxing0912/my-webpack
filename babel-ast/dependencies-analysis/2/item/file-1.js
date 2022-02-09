69b33d638b542d38bf1f9e8af76f28683891e60e
wuwenxing0912 <1428560819@qq.com>
2022-02-09 23:22:36 +0800

    修改文件类型

--- babel-ast/dependencies-analysis/2/item/file-1.js
+++ babel-ast/dependencies-analysis/2/item/file-1.js
@@ -1,4 +1,4 @@
-import obj1_1 from "./file-1-1/file-1-1.ts";
+import obj1_1 from "./file-1-1/file-1-1.js";
 const obj1 = {
   value: "obj1",
   value2: obj1_1,
--- babel-ast/dependencies-analysis/2/item/file-2-2/file-2-2.js
+++ babel-ast/dependencies-analysis/2/item/file-2-2/file-2-2.js
@@ -1,4 +1,4 @@
-import obj2_2_2 from "./file-2-2-2/file-2-2-2.ts";
+import obj2_2_2 from "./file-2-2-2/file-2-2-2.js";
 const obj2_2 = {
   value: "obj2",
   value2: obj2_2_2,
