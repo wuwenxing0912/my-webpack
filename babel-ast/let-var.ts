8b991489413f25c949d28f017676ee0a3b8a3968
wuwenxing0912 <1428560819@qq.com>
2022-02-09 21:33:58 +0800

    修改依赖

--- babel-ast/let-var.ts
+++ babel-ast/let-var.ts
@@ -1,5 +1,5 @@
 import { parse } from "@babel/parser"; //parse把ES6代码转换成AST
-import { traverse } from "@babel/core"; //traverse遍历AST进行修改
+import traverse from "@babel/traverse"; //traverse遍历AST进行修改
 import generate from "@babel/generator"; //generate把AST转换成ES5代码
 
 const code = `let string = 'string'; let number = 2;`;
