4652ee7b84938f479e944be6f1ade5836c9977f6
wuwenxing0912 <1428560819@qq.com>
2022-02-09 22:51:15 +0800

    循环依赖爆栈

---
+++ babel-ast/dependencies-analysis/3/item/file-1.ts
@@ -0,0 +1,6 @@
+import file_1 from "./file-2.ts";
+const obj1 = {
+  value: "obj1",
+  value2: file_1,
+};
+export default obj1;
---
+++ babel-ast/dependencies-analysis/3/item/file-2.ts
@@ -0,0 +1,6 @@
+import file_1 from "./file-1.ts";
+const obj2 = {
+  value: "obj2",
+  vaule: file_1,
+};
+export default obj2;
